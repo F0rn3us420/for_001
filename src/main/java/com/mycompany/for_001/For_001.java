4b1e89e8b1d89fc1c62103bba2121d26dc0bd08d
F0rn3us420 <andrescanchon020@gmail.com>
2025-04-06 20:43:37 -0500

    for_001

---
+++ src/main/java/com/mycompany/for_001/For_001.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.for_001;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class For_001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
